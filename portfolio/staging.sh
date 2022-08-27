7987fd26f147f28e02a811d9c90e8785fd4cc135
Li Yuxuan <44829092+xmliszt@users.noreply.github.com>
2022-08-28 02:15:03 +0800

    update deployment script

--- portfolio/staging.sh
+++ portfolio/staging.sh
@@ -4,8 +4,10 @@ set -e
 
 now=$(date +"%Y-%m-%d-%T")
 if [[ -d "dist" && -d "../../staging.xmliszt.github.io" ]]; then
+  rm -r ../../staging.xmliszt.github.io/*
   cp -r dist/* ../../staging.xmliszt.github.io/
   cd ../../staging.xmliszt.github.io
-  git checkout staging
-  git add -A && git commit -m "staging: $now" && git push origin staging
+  git checkout staging && git pull
+  git add -A && git commit -m "staging: $now"
+  git push origin staging
 fi
