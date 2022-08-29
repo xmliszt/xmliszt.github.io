8c5aa1da73992b5bbf9c84a2ac57c1709fc2f16c
Li Yuxuan <44829092+xmliszt@users.noreply.github.com>
2022-08-29 16:45:39 +0800

    templates for projects showcase inputs

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -11,7 +11,16 @@
     <section id="skills">
       <SkillsOverview />
     </section>
-    <section id="projects">PROJECTS</section>
+    <section id="projects">
+      <div>
+        <FishCard
+          class="fish-card"
+          v-for="(project, idx) in projects"
+          :key="idx"
+          :project="project"
+        />
+      </div>
+    </section>
   </main>
 </template>
 
@@ -19,17 +28,42 @@
 import AuthButton from "./components/AuthButton.vue";
 import ParallaxView from "./components/ParallaxView.vue";
 import SkillsOverview from "./components/SkillsOverview.vue";
+import FishCard from "./components/generic/FishCard.vue";
 
 export default {
   components: {
     AuthButton,
     ParallaxView,
     SkillsOverview,
+    FishCard,
   },
   data() {
     return {
       authBtnOpacity: 1,
       authBtnHidden: false,
+      projects: [
+        {
+          title: "Resume Generator",
+          details:
+            "Resume Generator produces resume based on your data uploaded. Writing resume cannot be simpler!",
+          imgUrl:
+            "https://images.unsplash.com/photo-1661630807560-654a6b3735bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80",
+        },
+        {
+          title: "Portfolio",
+          details:
+            "My very own portfolio website. A playground for CSS animations. I aim to achieve 'Apple-like' user experience.",
+          imgUrl:
+            "https://images.unsplash.com/photo-1556656793-08538906a9f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+        },
+        {
+          title: "iPhone 14 Simulator",
+          details:
+            "No money to buy an iPhone 14? No worries! This simulator will give you first-hand virtual experience of owning an iPhone 14.",
+          imgUrl:
+            "https://images.unsplash.com/photo-1523206489230-c012c64b2b48?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80",
+        },
+      ],
     };
   },
   methods: {
@@ -67,5 +101,22 @@ section {
 
 #skills {
   position: relative;
+}
+
+#projects {
+  padding: 25px;
+}
+
+#projects > div {
+  display: grid;
+  justify-content: center;
+  grid-template-columns: auto auto;
+  grid-gap: 2rem;
+}
+
+@media (max-width: 1024px) {
+  #projects > div {
+    grid-template-columns: auto;
+  }
 }
 </style>
---
+++ portfolio/src/components/generic/FishCard.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="fish-card">
+    <div class="fish-card-bg" :style="setBackgroundStyles()"></div>
+    <div class="glow"></div>
+    <div class="fish-card-overlay">
+      <span class="title">{{ project.title }}</span>
+      <span class="details">{{ project.details }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["project"],
+  methods: {
+    setBackgroundStyles() {
+      return {
+        background: `url(${this.project.imgUrl}) no-repeat center center`,
+        position: "absolute",
+        top: 0,
+        left: 0,
+        "background-size": "cover",
+        width: "100%",
+        height: "100%",
+        overflow: "hidden",
+        opacity: 0.7,
+        filter: "blur(0px)",
+        "-webkit-filter": "blur(0px)",
+        transition: "all 0.5s",
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.fish-card {
+  width: 35vw;
+  aspect-ratio: 1.67;
+  position: relative;
+  border-radius: 24px;
+  overflow: hidden;
+  box-shadow: var(--drop-shadow-dark) 0px 7px 29px 0px;
+  transition: box-shadow 0.5s ease-in-out;
+  background-color: var(--color-black);
+}
+
+.fish-card div.glow {
+  display: block;
+  position: absolute;
+  top: -100px;
+  width: 10%;
+  height: 200%;
+  background-color: rgba(255, 255, 255, 0.3);
+  transform: translate(-400%) rotate(20deg);
+  filter: blur(30px);
+  -webkit-filter: blur(30px);
+  transition: all 0.5s cubic-bezier(0.645, 0.045, 0.355, 1);
+}
+
+.fish-card div.fish-card-overlay {
+  box-sizing: border-box;
+  position: absolute;
+  height: 50%;
+  width: 100%;
+  bottom: -50%;
+  left: 0px;
+  background: rgb(2, 0, 36);
+  background: linear-gradient(
+    0deg,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(0, 0, 0, 0.7763480392156863) 37%,
+    rgba(0, 0, 0, 0) 100%
+  );
+  transition-property: bottom;
+  transition-duration: 300ms;
+  padding: 25px;
+}
+
+span {
+  display: block;
+}
+span.title {
+  font-size: var(--font-large);
+  margin-bottom: 20px;
+}
+
+span.detials {
+  font-size: var(--font-small);
+}
+
+@media (hover: hover) {
+  .fish-card:hover div.glow {
+    transform: translate(1100%);
+    transition: all 0.5s cubic-bezier(0.645, 0.045, 0.355, 1);
+  }
+
+  .fish-card:hover {
+    box-shadow: var(--drop-shadow-dark) 0px 0px 0px 0px;
+  }
+
+  .fish-card:hover div.fish-card-bg {
+    transform: scale(1.1);
+    filter: blur(2px);
+    -webkit-filter: blur(2px);
+  }
+
+  .fish-card:hover div.fish-card-overlay {
+    bottom: 0px;
+  }
+}
+
+@media (max-width: 1024px) {
+  .fish-card {
+    width: 80vw;
+  }
+
+  .fish-card div.fish-card-overlay {
+    height: 100%;
+    bottom: 0px;
+    background: linear-gradient(
+      0deg,
+      rgba(2, 0, 36, 1) 0%,
+      rgba(0, 0, 0, 0.5) 78%,
+      rgba(0, 0, 0, 0) 100%
+    );
+  }
+  .fish-card div.fish-card-bg {
+    filter: blur(1px);
+    -webkit-filter: blur(1px);
+  }
+}
+</style>
