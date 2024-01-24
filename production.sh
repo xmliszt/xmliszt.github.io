52bf0d97049c4dd14717c3eb52950842263f66e8
Li Yuxuan <44829092+xmliszt@users.noreply.github.com>
2024-01-24 22:43:37 +0800

    remove staging

--- production.sh
+++ production.sh
@@ -5,7 +5,7 @@ now=$(date +"%Y-%m-%d-%T")
 if [[ -d "dist" ]]; then
   cd dist
   git init
-  git remote add origin git@github.com:xmliszt/xmliszt.github.io.git
+  git remote add origin git@github.com:xmliszt/liyuxuan.dev.git
   git add -A
   git commit -m "production: $now"
   git push -f origin main:master
